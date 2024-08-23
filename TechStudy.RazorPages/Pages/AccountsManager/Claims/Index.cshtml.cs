f0b69bd2875bc885feb6838bf2ca109d9e85033e
NizoDotNet <mmmdovnizami.official@gmail.com>
2024-08-23 16:28:19 +0400

    Fixed Delete bug in Accountsmanager

--- TechStudy.RazorPages/Pages/AccountsManager/Claims/Index.cshtml.cs
+++ TechStudy.RazorPages/Pages/AccountsManager/Claims/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 using TechStudy.RazorPages.Services;
 
 namespace TechStudy.RazorPages.Pages.AccountsManager.Claims
@@ -8,7 +9,7 @@ namespace TechStudy.RazorPages.Pages.AccountsManager.Claims
     public class IndexModel : PageModel
     {
         private readonly IUserService _userService;
-        
+
         public IndexModel(IUserService userService)
         {
             _userService = userService;
@@ -21,7 +22,12 @@ namespace TechStudy.RazorPages.Pages.AccountsManager.Claims
             Users = await _userService.GetAllAsync();
 
             return Page();
-            
+
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync()
+        {
+
         }
     }
 }
